045d597f24e286769b4388edca925aadfed42eba
coregenxdigital <coregenxdigital@gmail.com>
2026-02-03 16:47:08 +0530

    Initial commit

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -54,7 +54,7 @@ export function Home() {
     setIsLoading(true)
 
     try {
-      const response = await axios.post('http://localhost:3000/api/chat', {
+      const response = await axios.post('https://chatbot-node-two.vercel.app/api/chat', {
         message: userInput
       })
 
